91b00c4ee0639333025be058d76a154336011e7e
lvxn0va <lvx.n0va@gmail.com>
2012-10-28 14:10:39 -0700

    Customize seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,18 @@
 puts 'CREATING ROLES'
 Role.create([
   { :name => 'admin' }, 
-  { :name => 'user' }, 
-  { :name => 'VIP' }
+  { :name => 'trial' }, 
+  { :name => 'monthly' },
+  { :name => 'yearly' }
 ], :without_protection => true)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << user2.name
+user = User.create! :name => 'Thespee Admin', :email => 'thespee.site@gmail.com', :password => '7823bashar!teg3', :password_confirmation => '7823bashar!teg3'
 user.add_role :admin
-user2.add_role :VIP
+puts 'admin created: ' << user.name
+user2 = User.create! :name => 'trial', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+user2.add_role :silver
+user3 = User.create! :name => 'monthly', :email => 'user3@example.com', :password => 'please', :password_confirmation => 'please'
+user3.add_role :gold
+user4 = User.create! :name => 'yearly', :email => 'user4@example.com', :password => 'please', :password_confirmation => 'please'
+user4.add_role :platinum
+puts "added users: #{user2.name}, #{user3.name}, #{user4.name}"
