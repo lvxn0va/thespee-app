287d1e6e07a5bb16e01c164cfd2bb230d99bfb08
lvxn0va <lvx.n0va@gmail.com>
2012-10-28 17:12:02 -0700

    add authorization

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,10 +1,15 @@
 class ContentController < ApplicationController
+  before_filter :authenticate_user!
+  
   def monthly
+    authorize! :view, :all, :message => "Sorry access limited to plan subscribers only."
   end
 
   def quarterly
+    authorize! :view, :all, :message => "Sorry access limited to plan subscribers only."
   end
 
   def yearly
+    authorize! :view, :all, :message => "Sorry access limited to plan subscribers only."
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,10 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.has_role? :admin
       can :manage, :all
+    else
+      can :read, :all if user.has_role? :monthly
+      can :read, :all if user.has_role? :quarterly
+      can :read, :all if user.has_role? :yearly
     end
     # Define abilities for the passed in user here. For example:
     #
