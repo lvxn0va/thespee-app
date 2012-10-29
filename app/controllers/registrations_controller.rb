f11f834c1b5a7486f7cd727991d12c0d6321e6ee
lvxn0va <lvx.n0va@gmail.com>
2012-10-28 23:20:32 -0700

    accomodate changes to account

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
   
-  def new
-    @plan = params[:plan]
-    super
-  end
+   def new
+      @plan = params[:plan]
+      if @plan
+        super
+      else
+        redirect_to root_path, :notice => 'Please select a subscription plan below'
+      end
+    end
+
+    def update
+      role = Role.find(params[:user][:role_ids]) unless params[:user][:role_ids].nil?
+      params[:user] = params[:user].except(:role_ids)
+      super
+      resource.update_plan(role) unless role.nil?
+    end
 
   private
   def build_resource(*args)
